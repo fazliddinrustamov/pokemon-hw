2979b0f419a46440120cea13d82e8c61de39d15b
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-08-23 20:58:54 +0500

    add hw codes!

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var elMainList = $('.main-list');
+
+pokemons.forEach(pokemon => {
+  let partItem = createElement('li', 'part-item d-flex flex-column mb-5');
+  let itemHeading = createElement('h3', 'pokemon-heading', `${pokemon.name}`);
+  let itemImg = createElement('img', 'pokemon-img mb-3');
+  itemImg.src = pokemon.img;
+  itemImg.height = 150;
+  itemImg.width = 150;
+  let itemType = createElement('p', 'pokemon-type m-0', `Type: ${pokemon.type}`);
+  let itemWidth = createElement('span', 'pokemon-width', `Width: ${pokemon.weight}`);
+  let itemHeight = createElement('span', 'pokemon-height', `Height: ${pokemon.height}`);
+  let itemCandy = createElement('span', 'pokemon-candy', `Candy: ${pokemon.candy}`);
+  let itemCandyCount = createElement('span', 'pokemon-count', `Candy count: ${pokemon.candy_count}`);
+  let itemEgg = createElement('span', 'pokemon-egg', `Egg: ${pokemon.egg}`);
+  let itemSpawnChance = createElement('span', 'pokemon-chance', `Spawn chance: ${pokemon.spawn_chance}`);
+  let itemAvgSpavns = createElement('span', 'pokemon-avg', `Avg Spawns: ${pokemon.avg_spawns}`);
+  let itemSpawnTime = createElement('span', 'pokemon-time', `Spawns time: ${pokemon.spawn_time}`);
+  let itemMultipliers = createElement('span', 'pokemon-multipliers', `Multipliers: ${pokemon.multipliers}`);
+  let itemWeaknesses = createElement('span', 'pokemon-weaknesses', `Weaknesses: ${pokemon.weaknesses}`);
+  
+  partItem.append(itemHeading, itemImg, itemType, itemWidth, itemHeight, itemCandy, itemCandyCount, itemEgg, itemSpawnChance, itemAvgSpavns, itemSpawnTime, itemMultipliers, itemWeaknesses);
+  elMainList.appendChild(partItem);
+})
+
+// for (var pokemon of pokemons) {
+//   let partItem = createElement('li', 'part-item d-flex flex-column mb-5');
+//   let itemHeading = createElement('h3', 'pokemon-heading', `${pokemon.name}`);
+//   let itemImg = createElement('img', 'pokemon-img mb-3');
+//   itemImg.src = pokemon.img;
+//   itemImg.height = 150;
+//   itemImg.width = 150;
+//   let itemType = createElement('p', 'pokemon-type m-0', `Type: ${pokemon.type}`);
+//   let itemWidth = createElement('span', 'pokemon-width', `Width: ${pokemon.weight}`);
+//   let itemHeight = createElement('span', 'pokemon-height', `Height: ${pokemon.height}`);
+//   let itemCandy = createElement('span', 'pokemon-candy', `Candy: ${pokemon.candy}`);
+//   let itemCandyCount = createElement('span', 'pokemon-count', `Candy count: ${pokemon.candy_count}`);
+//   let itemEgg = createElement('span', 'pokemon-egg', `Egg: ${pokemon.egg}`);
+//   let itemSpawnChance = createElement('span', 'pokemon-chance', `Spawn chance: ${pokemon.spawn_chance}`);
+//   let itemAvgSpavns = createElement('span', 'pokemon-avg', `Avg Spawns: ${pokemon.avg_spawns}`);
+//   let itemSpawnTime = createElement('span', 'pokemon-time', `Spawns time: ${pokemon.spawn_time}`);
+//   let itemMultipliers = createElement('span', 'pokemon-multipliers', `Multipliers: ${pokemon.multipliers}`);
+//   let itemWeaknesses = createElement('span', 'pokemon-weaknesses', `Weaknesses: ${pokemon.weaknesses}`);
+  
+//   partItem.append(itemHeading, itemImg, itemType, itemWidth, itemHeight, itemCandy, itemCandyCount, itemEgg, itemSpawnChance, itemAvgSpavns, itemSpawnTime, itemMultipliers, itemWeaknesses);
+//   elMainList.appendChild(partItem);
+// }
---
+++ js/utilities.js
@@ -0,0 +1,12 @@
+let $ = function(selector, node = document) {
+  return node.querySelector(selector);
+}
+
+let createElement = function(tagName, className = "", text) {
+  let element = document.createElement(tagName);
+  element.setAttribute("class", className);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+}
